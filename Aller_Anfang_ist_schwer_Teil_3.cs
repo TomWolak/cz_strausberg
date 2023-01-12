dd0055d8bd49bf6430ead97b2619b1e48c517da5
TomWolak <122091162+TomWolak@users.noreply.github.com>
2023-01-12 14:50:28 +0100

    Add files via upload

---
+++ Aller_Anfang_ist_schwer_Teil_3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aller_Anfang_ist_schwer_Teil_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ergebnis = 0;
+            int produkt = 128 * 512;
+
+            Console.WriteLine("Löse der Aufagebe 128 * 512");
+            Console.ReadLine();
+
+            for (int i = 128; i <= produkt; i++)
+            {
+                ergebnis = i;
+                Console.WriteLine(ergebnis);
+                
+                if (i > 60000)
+                {
+                    i = produkt;
+                }
+            }
+
+            Console.WriteLine("Die 'for' Schleife ist mit dem Wert: " + ergebnis + " beendet");
+
+            Console.ReadLine();
+
+        }
+    }
+}
